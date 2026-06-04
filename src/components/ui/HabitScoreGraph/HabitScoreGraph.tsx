064e8c55f65e9b51c19cd7661222658eaa2677b4
al-soup <al@soup.one>
2026-06-04 19:44:00 +0200

    feat: improve a11y for habit gird

--- src/components/ui/HabitScoreGraph/HabitScoreGraph.tsx
+++ src/components/ui/HabitScoreGraph/HabitScoreGraph.tsx
@@ -179,7 +179,7 @@ export function HabitScoreGraph({
 
             <div
               className={styles.grid}
-              role="grid"
+              role="img"
               aria-label="Habit score graph"
             >
               {days.map((day) => (
